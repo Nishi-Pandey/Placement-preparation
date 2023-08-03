9072d8f62e3d7b8a72a6cb39d5f5f7df4524f543
Nishi-Pandey <135330433+Nishi-Pandey@users.noreply.github.com>
2023-08-03 23:01:44 +0530

    Time: 286 ms (93.67%), Space: 116.8 MB (81.54%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,37 @@
+#include <queue>
+
+class MedianFinder {
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if (maxHeap.empty() || num <= maxHeap.top()) {
+            maxHeap.push(num);
+        } else {
+            minHeap.push(num);
+        }
+        
+        // Balance the heaps
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            return maxHeap.top();
+        }
+    }
+
+private:
+    std::priority_queue<int> maxHeap; // Max heap for the lower half
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap; // Min heap for the upper half
+};
