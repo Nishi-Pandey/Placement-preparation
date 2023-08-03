0613c23f0cafc787f6862d7dc66ed5bae1508831
Nishi-Pandey <135330433+Nishi-Pandey@users.noreply.github.com>
2023-08-04 00:07:28 +0530

    Time: 3 ms (94.10%), Space: 8.9 MB (55.62%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <unordered_map>
+#include <stack>
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> nextGreater;
+        stack<int> st;
+
+        // Traverse nums2 in reverse order
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                nextGreater[nums2[i]] = st.top();
+            } else {
+                nextGreater[nums2[i]] = -1;
+            }
+
+            st.push(nums2[i]);
+        }
+
+        vector<int> result;
+        for (int num : nums1) {
+            result.push_back(nextGreater[num]);
+        }
+
+        return result;
+    }
+};
